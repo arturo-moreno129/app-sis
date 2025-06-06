c1ddde53421442f770baded19c7821c6e7fe4a8d
Jose Arturo Moreno Aguilar <JMoreno@PC-AuxSistemas.acr.com.mx>
2025-06-06 11:55:28 -0600

    .

--- js/app.js
+++ js/app.js
@@ -14,6 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
   signin.addEventListener('click', (event) => {
     event.preventDefault(); // Evita que el formulario se envíe
-    window.location.assign("loading.html");
+    window.location.assign("modulos/loading.html");
   });
 });
