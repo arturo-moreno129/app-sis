195d178e1a1ac3eb7065c9dc95d357440851fdc5
Jose Arturo Moreno Aguilar <JMoreno@PC-AuxSistemas.acr.com.mx>
2025-06-26 17:21:17 -0600

    wip:se creo menu lateral

--- modulos/main.php
+++ modulos/main.php
@@ -8,89 +8,58 @@ if (!isset($_SESSION['nombre'])) {
 }*/
 ?>
 <!-- Contenido visible solo para usuarios autenticados -->
-
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://kit.fontawesome.com/258c34b44b.js" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <title>ACR</title>
+    <link rel="stylesheet" href="../css/styleModulos.css">
+    <link rel="shortcut icon" href="../img/icon.png" type="image/x-icon">
 </head>
 
 <body>
-    <div class="sidebar">
-        <div class="sidebar-brand">
-            <h1><span class="lab la-accusoft"></span>Accusoft</h1>
-        </div>
-        <div class="sidebar-menu">
+    <div class="container">
+        <div class="navigation">
             <ul>
                 <li>
-                    <a href="" class="las la-igloo"></a>
-                    <span>Dashboard</span>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="bus-outline"></ion-icon></span>
+                        <span class="title">Camiones rivera</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="" class="las la-user"></a>
-                    <span>Dashboard</span>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="home-outline"></ion-icon></span>
+                        <span class="title">Dasboard</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="" class="las la-clipboard-list"></a>
-                    <span>Dashboard</span>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="person-outline"></ion-icon></span>
+                        <span class="title">Usuarios</span>
+                    </a>
                 </li>
                 <li>
-                    <a href="" class="las la-receipt"></a>
-                    <span>Dashboard</span>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="ticket-outline"></ion-icon></span>
+                        <span class="title">Tickets</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        <span class="icon"><ion-icon name="ticket-outline"></ion-icon></span>
+                        <span class="title">Help</span>
+                    </a>
                 </li>
             </ul>
         </div>
     </div>
-    <div class="main-conten">
-        <header>
-            <h1>
-                <label for="">
-                    <span class="las la-bars"></span>
-                </label>
-                Dashboard
-            </h1>
-            <div class="search-wrpper">
-                <span class="las la-search"></span>
-                <input type="search" placeholder="Search here">
-            </div>
-            <div class="user-wrapper">
-                <img src="../img/icon.png" width="30px" height="30px" alt="">
-                <div>
-                    <h4>Jose Arturo</h4>
-                    <small>Admin</small>
-                </div>
-            </div>
-        </header>
-        <main>
-            <div class="cards">
-                <div class="card-single">
-                    <div>
-                        <h1>54</h1>
-                        <span>Custumers</span>
-                    </div>
-                    <div>
-                        <span class="las la-users"></span>
-                    </div>
-                </div>
-                <div class="card-single">
-                    <div>
-                        <h1>54</h1>
-                        <span>Custumers</span>
-                    </div>
-                    <div>
-                        <span class="las la-users"></span>
-                    </div>
-                </div>
-            </div>
-        </main>
-    </div>
+    <footer>
+        <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+    </footer>
 </body>
-<footer>
-    <script src="../js/app.js"></script>
-</footer>
 
 </html>
